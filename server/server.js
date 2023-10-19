529046686c323ba4336d65296af9cf87b082a587
Cong Guo <jeraviah@gmail.com>
2023-10-19 22:39:13 +1300

    Init data file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
+import fs from 'node:fs/promises'
 import * as Path from 'node:path'
-// import * as URL from 'node:url'
-
+import * as URL from 'node:url'
 import express from 'express'
 import hbs from 'express-handlebars'
 
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
+
+// Read file
+async function readData() {
+  const data = await fs.readFile(Path.join(__dirname, `./data/data.json`), {
+    encoding: 'utf-8',
+  })
+  return JSON.parse(data)
+}
 
 const server = express()
 
@@ -17,17 +27,46 @@ server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 server.set('views', Path.resolve('server/views'))
 
-// Your routes/router(s) should go here
+// routes/router(s)
 server.get('/', (req, res) => {
   res.render('home')
 })
 
-server.post('/', (req, res) => {
+server.post('/', async (req, res) => {
+  let data = await readData()
+
+  let idArr = []
+  for (let i=0; i<data.length; i++) {
+    idArr.push(data[i]['id'])
+  }
+  let maxId
+  if (idArr.length>0) {maxId = Math.max(...idArr)}else{maxId=0}
+  const newId = maxId + 1
+  const newAppointment = {
+    id: newId,
+    ...req.body
+  }
+  data.push(newAppointment)
+
+  await fs.writeFile(
+    Path.join(__dirname, `./data/data.json`),
+    JSON.stringify(data, null, 2),
+    {
+      encoding: 'utf-8',
+    }
+  )
+
   res.redirect('appointments')
 })
 
-server.get('/appointments', (req, res) => {
-  res.send('This will be appointments history page')
+server.get('/appointments', async (req, res) => {
+  const template = 'appointments'
+  let data = await readData()
+  const viewData = {
+    appointments: data
+  }
+  
+  res.render(template, viewData)
 })
 
 export default server
